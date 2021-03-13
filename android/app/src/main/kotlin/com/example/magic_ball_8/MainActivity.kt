be089734f731db895870b5bc31dbe7ca7698d593
Darshit Rudani <darshit112001@users.noreply.github.com>
2021-03-13 10:37:13 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/magic_ball_8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.magic_ball_8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}

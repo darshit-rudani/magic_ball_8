0fa6753f2728bb40fef5093f0b9885bb38365dcd
Darshit Rudani <darshit112001@users.noreply.github.com>
2021-03-13 10:44:09 +0530

    add AppBar and Function for body

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,36 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.indigo.shade700,
+          title: Center(
+            child: Text(
+              'Ask Me Anything.',
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'RocknRollOne',
+              ),
+            ),
+          ),
+        ),
+        body: Container(
+          child: MyApp(),
+        ),
+      ),
     ),
   );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  // ignore: missing_return
+  Future build(BuildContext context) async {}
 }
